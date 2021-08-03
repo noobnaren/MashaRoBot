0d89d5aab386641096c391bb1e3d8a2d540781b4
noobnaren <87747387+noobnaren@users.noreply.github.com>
2021-08-03 19:50:38 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -57,12 +57,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
+    if calc == 1:
         pairs.append((modules[-1], ))
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 10)
+    max_num_pages = ceil(len(pairs) / 8)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
@@ -73,7 +73,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("⧐", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="masha_back")]]
 
 
 
